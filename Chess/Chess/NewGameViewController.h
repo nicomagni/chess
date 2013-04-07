c2ee681ef0676c4d042c403977dca87a6ef70371
Nico Magni <nicomagni@wolox.com.ar>
2013-04-07 14:14:50 -0300

    adding the iPhone story board and the ipad storyboard. Basic flow finished.
    Signed-off-by: Nico Magni <nicomagni@wolox.com.ar>

---
+++ Chess/Chess/NewGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewGameViewController.h
+//  Chess
+//
+//  Created by Nico Magni on 4/7/13.
+//  Copyright (c) 2013 Nico Magni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface NewGameViewController : ViewController
+
+@end
